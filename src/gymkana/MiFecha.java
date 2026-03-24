08be17563a5322237ddd0df2fd36cb819aefa496
“mickaeldam” <mikechaudam@gmail.com>
2026-03-24 20:47:09 +0100

    Ejercicio ginkana 2 y 3  sin terminar

--- src/gymkana/MiFecha.java
+++ src/gymkana/MiFecha.java
@@ -1,10 +1,13 @@
 package gymkana;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.TextStyle;
+import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 
 public class MiFecha {
+	
 
 	public static void main(String[] args) {
 		
@@ -20,6 +23,33 @@ public class MiFecha {
 		System.out.println("Mes: " + miFechaNacimiento.getMonth().getDisplayName(TextStyle.FULL, idioma).toUpperCase());
 		System.out.println("Día: " + miFechaNacimiento.getDayOfWeek().getDisplayName(TextStyle.FULL, idioma).toUpperCase());
 		
+		//Fase 2 Mi edad
+		
+		LocalDate fechaHoy = LocalDate.now();
+		Period edad =Period.between(miFechaNacimiento, fechaHoy);
+		
+		System.out.println("Fecha de nacimiento: " + miFechaNacimiento);
+		System.out.println("Fecha de hoy " + fechaHoy);
+		System.out.println("Edad exacta: " + edad.getYears() + " años, " + edad.getMonths() + " meses y " + edad.getDays() + " diás" );
+		System.out.println("Total días vividos: " + ChronoUnit.DAYS.between(miFechaNacimiento, fechaHoy));
+		System.out.println("Semanas completas: " + ChronoUnit.WEEKS.between(miFechaNacimiento, fechaHoy));
+		
+		//Fase 3 Proximo cumple
+		
+		miFechaNacimiento = miFechaNacimiento.withYear(2026);
+		enum DiaFinSemana{
+			SATURDAY,
+			SUNDAY
+		}
+		
+		System.out.println("Tu proximo cumpleaños " + miFechaNacimiento);
+		System.out.println("Dias que faltan: " + ChronoUnit.DAYS.between(fechaHoy, miFechaNacimiento));
+		System.out.println("Caera en: " + miFechaNacimiento.getDayOfWeek().getDisplayName(TextStyle.FULL,idioma).toUpperCase() );
+		
+		if( miFechaNacimiento.getDayOfWeek() != DiaFinSemana) {
+			System.out.println();
+		}
+		System.out.println("Tipo de dia " + miFechaNacimiento.DayOf);
 
 	}
 
