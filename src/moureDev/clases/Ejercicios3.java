1b4fa069a07c6fedfa391a80a948d6c2f4153520
“mickaeldam” <mikechaudam@gmail.com>
2026-04-15 21:01:03 +0200

    operadores moure dev

--- src/moureDev/clases/Ejercicios3.java
+++ src/moureDev/clases/Ejercicios3.java
@@ -33,7 +33,52 @@ public class Ejercicios3 {
 		e %= e;
 		System.out.println(e);
 		
+		//3 Imprime tres comparaciones verdaderas con diferentes operadores de comparacion
+		System.out.println(5 > 3);
+		System.out.println(6 < 10);
+		System.out.println(10 == 10);
+		//4 Imprime tres comparaciones falsas con diferentes operadores de comparacion 
+		System.out.println(5 > 6);
+		System.out.println(10 < 6);
+		System.out.println(10 != 10);
+		
+		//5 Utiliza el operador logico and 
+		System.out.println("Los primeros numeros son mayores? " + (c > a && c > b));
+		System.out.println("Segunda vuelta Los primeros numeros son mayores? " + (e > a && e > b));
 
+		//6 Utiliza el operador logico or 
+		System.out.println("Alguna varible es mayor que 0? "+ (b > 0 || e > 0));
+		System.out.println("Segunda vuelta, alguna varible es mayor que 0? "+ (b > 0 || d > 0));
+		
+		//7 Combina ambos operadores logicos 
+		//"El acceso VIP a la discoteca"
+		int edad = 17;
+		boolean tieneEntrada = false;
+		boolean esHijoDelDueño = true;
+		boolean estaEnListaNegra = false;
+		
+		System.out.println("Puedes ingresar a la zona VIP? " + ( ((edad >= 18 && tieneEntrada) || esHijoDelDueño ) && !estaEnListaNegra));
+		//refactorizamos para organizarlo un poco mejor
+		//8 Añade una negacion, ya lo hemos realizado  
+		boolean cumpleRequisitos = (edad >= 18 && tieneEntrada) || esHijoDelDueño;
+		boolean puedePasar = cumpleRequisitos && !estaEnListaNegra;
+		System.out.println("¿acceso consedido? " + puedePasar);
+		
+		//9 Imprime 3 ejemplos de uso de operadores unarios 
+		int num1 = 5;
+		int num2 = 5;
+		boolean esPrimavera = true;
+		boolean esInvierno = false;
+		//incremento y decremento 
+		System.out.println(num1++);
+		System.out.println(num1--);
+		System.out.println(num1);
+		//Cambio de signo 
+		System.out.println(-num2);
+		System.out.println(+num2);
+		//Complemento logico
+		System.out.println(!esPrimavera);
+		System.out.println(!esInvierno);
 	}
 
 }
