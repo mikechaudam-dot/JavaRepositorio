f35f85ec0827f0d9b25b3b73bfd0d1689c256cdb
“mickaeldam” <mikechaudam@gmail.com>
2026-04-28 20:59:52 +0200

    Inicio ejercicio encapsulamiento

--- src/moureDevPoo/Encapsulamiento.java
+++ src/moureDevPoo/Encapsulamiento.java
@@ -3,7 +3,9 @@ package moureDevPoo;
 public class Encapsulamiento {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 1 - Crea una clase person y establece atributos privados de name y age 
+		//y usa los metodos getName(), setName(), getAge() y setAge() para asignar 
+		//y mostrar valores desde otra clase
 
 	}
 
--- src/moureDevPoo/Person.java
+++ src/moureDevPoo/Person.java
@@ -1,7 +1,16 @@
 package moureDevPoo;
 
+//1 - Crea una clase person y establece atributos privados de name y age 
+		//y usa los metodos getName(), setName(), getAge() y setAge() para asignar 
+		//y mostrar valores desde otra clase
+
 public class Person {
 	private String name;
 	private int age;
+	
+	public Person(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
 
 }
