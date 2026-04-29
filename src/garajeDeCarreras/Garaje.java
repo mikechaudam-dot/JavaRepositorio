6f80679a4b113e6d92d5bc2b12271fd8c232ae92
mikechaudam <mikechaudam@gmail.com>
2026-04-29 15:29:03 +0200

    comentarios main garaje

--- src/garajeDeCarreras/Garaje.java
+++ src/garajeDeCarreras/Garaje.java
@@ -4,10 +4,10 @@ public class Garaje {
 
 	public static void main(String[] args) {
 		
+		//Instanciamos los dos objetos
 		Engine electrico = new Engine(100.0,"Electrico"); 
-		
 		RacingCar corvete = new RacingCar("corvete", electrico, 100);
-		
+		//accedemos al metodo start del engine 
 		corvete.getEngine().start();
 
 	}
